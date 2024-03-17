91390acc9a334f9af5402e15a382a0cb195a6adb
Souparna Bhowmik <jsouparna2@gmail.com>
2024-03-17 15:29:17 +0530

    First commit to github

---
+++ src/app/data.js
@@ -0,0 +1,105 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "Online Canvas",
+    description: "Draw on a canvas",
+    date: "2021-04-15",
+    demoLink: "https://souparna21.github.io/online-canvas/",
+  },
+  {
+    id: 2,
+    name: "Painting on air",
+    description: "Digital art showcase platform",
+    date: "2021-06-20",
+    demoLink: "https://github.com/souparna21/paint-by-hand",
+  },
+  // {
+  //   id: 3,
+  //   name: "BudgetPlanner",
+  //   description: "Plan and track expenses",
+  //   date: "2022-09-10",
+  //   demoLink: "https://budgetplanner.example.com",
+  // },
+  // {
+  //   id: 4,
+  //   name: "HealthBeat",
+  //   description: "Monitor heart rate zones",
+  //   date: "2022-05-30",
+  //   demoLink: "https://healthbeat.example.com",
+  // },
+  // {
+  //   id: 5,
+  //   name: "RecipeFinder",
+  //   description: "Discover new recipes",
+  //   date: "2022-07-12",
+  //   demoLink: "https://recipefinder.example.com",
+  // },
+  // {
+  //   id: 6,
+  //   name: "JourneyLogger",
+  //   description: "Log your travels",
+  //   date: "2022-10-01",
+  //   demoLink: "https://journeylogger.example.com",
+  // },
+  // {
+  //   id: 7,
+  //   name: "StudyBuddy",
+  //   description: "Collaborative learning platform",
+  //   date: "2022-04-18",
+  //   demoLink: "https://studybuddy.example.com",
+  // },
+  // {
+  //   id: 8,
+  //   name: "TechTalk",
+  //   description: "Tech news aggregator",
+  //   date: "2022-11-05",
+  //   demoLink: "https://techtalk.example.com",
+  // },
+  // {
+  //   id: 9,
+  //   name: "FitTrack",
+  //   description: "Fitness and workout tracker",
+  //   date: "2022-03-22",
+  //   demoLink: "https://fittrack.example.com",
+  // },
+  // {
+  //   id: 10,
+  //   name: "MindfulMoments",
+  //   description: "Meditation and mindfulness app",
+  //   date: "2022-02-14",
+  //   demoLink: "https://mindfulmoments.example.com",
+  // },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/souparna21",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/souparna-bhowmik/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://twitter.com/BhowmikSou97931",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/resume.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
